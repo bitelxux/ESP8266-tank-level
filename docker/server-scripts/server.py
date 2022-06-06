240b0f8d528cd42d20108b8439952eadd6c45246
cnn <cnn@zoe.local.com>
2022-06-06 22:26:20 +0200

    [23] points

--- docker/server-scripts/server.py
+++ docker/server-scripts/server.py
@@ -34,7 +34,7 @@ def add(value):
               }
           }
         ]
-        client.write_point(body)
+        client.write_points(body)
         return f"The value {value} has been gladly added"
     except:
         return f"Error adding {value}"
